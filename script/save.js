18c30d240811cd65d85468bde9c7bcba0b11297a
Fantom42 <139061392+Fantom42@users.noreply.github.com>
2024-04-10 00:28:45 +0200

    Update save.js

--- script/save.js
+++ script/save.js
@@ -7,6 +7,7 @@ function player_data_save() {
         ,passive_perception: document.getElementById("pass_percep").value 
         ,profficiency_bonus: document.getElementById("prof_bonus").value 
         ,initiative: document.getElementById("initiative").value
+        ,ac: document.getElmentById("ac").value
     },{
         str: document.getElementById("str").value 
         ,str_bonus: document.getElementById("str_bonus").value 
@@ -77,69 +78,70 @@ function player_data_load(){
     let fileread = new FileReader();
     fileread.onload = function (e) {
         let content = e.target.result;
-        var intern = JSON.parse(content);
+        var stat = JSON.parse(content);
 
-        document.getElementById("name").value = intern[0].name
-        document.getElementById("race").value = intern[0].race
-        document.getElementById("class").value = intern[0].character_class
-        document.getElementById("mhp").value = intern[0].maxhp
-        document.getElementById("pass_percep").value = intern[0].passive_perception
-        document.getElementById("prof_bonus").value = intern[0].profficiency_bonus
-        document.getElementById("initiative").value = intern[0].initiative
+        document.getElementById("name").value = stat[0].name
+        document.getElementById("race").value = stat[0].race
+        document.getElementById("class").value = stat[0].character_class
+        document.getElementById("mhp").value = stat[0].maxhp
+        document.getElementById("pass_percep").value = stat[0].passive_perception
+        document.getElementById("prof_bonus").value = stat[0].profficiency_bonus
+        document.getElementById("initiative").value = stat[0].initiative
+        document.getElementById("ac").value = stat[0].initiative
     
-        document.getElementById("str").value = intern[1].str
-        document.getElementById("str_bonus").value = intern[1].str_bonus
-        document.getElementById("athl").value = intern[1].athletics
-        document.getElementById("int").value = intern[1].int
-        document.getElementById("int_bonus").value = intern[1].int_bonus
-        document.getElementById("arc").value = intern[1].arcana
-        document.getElementById("his").value = intern[1].history
-        document.getElementById("inv").value = intern[1].investigation
-        document.getElementById("nat").value = intern[1].nature
-        document.getElementById("reli").value = intern[1].religion
-        document.getElementById("wis").value = intern[1].wis
-        document.getElementById("wis_bonus").value = intern[1].wis_bonus
-        document.getElementById("anhan").value = intern[1].animal_handling
-        document.getElementById("ins").value = intern[1].insight
-        document.getElementById("med").value = intern[1].medicine
-        document.getElementById("percep").value = intern[1].perception
-        document.getElementById("surv").value = intern[1].survival
-        document.getElementById("dex").value = intern[1].dex
-        document.getElementById("dex_bonus").value = intern[1].dex_bonus
-        document.getElementById("acro").value = intern[1].acrobatics
-        document.getElementById("stea").value = intern[1].stealth
-        document.getElementById("soh").value = intern[1].sleight_of_hand
-        document.getElementById("cha").value = intern[1].cha
-        document.getElementById("cha_bonus").value = intern[1].cha_bonus
-        document.getElementById("pers").value = intern[1].persuation
-        document.getElementById("dec").value = intern[1].deception
-        document.getElementById("inti").value = intern[1].intimidation
-        document.getElementById("perf").value = intern[1].performance
-        document.getElementById("con").value = intern[1].con
-        document.getElementById("con_bonus").value = intern[1].con_bonus
+        document.getElementById("str").value = stat[1].str
+        document.getElementById("str_bonus").value = stat[1].str_bonus
+        document.getElementById("athl").value = stat[1].athletics
+        document.getElementById("int").value = stat[1].int
+        document.getElementById("int_bonus").value = stat[1].int_bonus
+        document.getElementById("arc").value = stat[1].arcana
+        document.getElementById("his").value = stat[1].history
+        document.getElementById("inv").value = stat[1].investigation
+        document.getElementById("nat").value = stat[1].nature
+        document.getElementById("reli").value = stat[1].religion
+        document.getElementById("wis").value = stat[1].wis
+        document.getElementById("wis_bonus").value = stat[1].wis_bonus
+        document.getElementById("anhan").value = stat[1].animal_handling
+        document.getElementById("ins").value = stat[1].insight
+        document.getElementById("med").value = stat[1].medicine
+        document.getElementById("percep").value = stat[1].perception
+        document.getElementById("surv").value = stat[1].survival
+        document.getElementById("dex").value = stat[1].dex
+        document.getElementById("dex_bonus").value = stat[1].dex_bonus
+        document.getElementById("acro").value = stat[1].acrobatics
+        document.getElementById("stea").value = stat[1].stealth
+        document.getElementById("soh").value = stat[1].sleight_of_hand
+        document.getElementById("cha").value = stat[1].cha
+        document.getElementById("cha_bonus").value = stat[1].cha_bonus
+        document.getElementById("pers").value = stat[1].persuation
+        document.getElementById("dec").value = stat[1].deception
+        document.getElementById("inti").value = stat[1].intimidation
+        document.getElementById("perf").value = stat[1].performance
+        document.getElementById("con").value = stat[1].con
+        document.getElementById("con_bonus").value = stat[1].con_bonus
     
-        document.getElementById("traits").value = intern[1].traits
+        document.getElementById("traits").value = stat[1].traits
     
-        document.getElementById("plat").value = intern[2].plat
-        document.getElementById("gold").value = intern[2].gold
-        document.getElementById("elec").value = intern[2].elec
-        document.getElementById("silv").value = intern[2].silv
-        document.getElementById("copp").value = intern[2].copp
-        document.getElementById("prof").value  = intern[2].profficiencies
-        document.getElementById("invent").value = intern[2].inventory
+        document.getElementById("plat").value = stat[2].plat
+        document.getElementById("gold").value = stat[2].gold
+        document.getElementById("elec").value = stat[2].elec
+        document.getElementById("silv").value = stat[2].silv
+        document.getElementById("copp").value = stat[2].copp
+        document.getElementById("prof").value  = stat[2].profficiencies
+        document.getElementById("invent").value = stat[2].inventory
 
-        document.getElementById("spell_save").value = intern[3].spellsave
-        document.getElementById("spell_atk_bonus").value = intern[3].spellattack
-        document.getElementById("cantrips").value = intern[3].cantrips
-        document.getElementById("lvl1spells").value = intern[3].lvl1spells
-        document.getElementById("lvl2spells").value = intern[3].lvl2spells
-        document.getElementById("lvl3spells").value = intern[3].lvl3spells
-        document.getElementById("lvl4spells").value = intern[3].lvl4spells
-        document.getElementById("lvl5spells").value = intern[3].lvl5spells
-        document.getElementById("lvl6spells").value = intern[3].lvl6spells
-        document.getElementById("lvl7spells").value = intern[3].lvl7spells
-        document.getElementById("lvl8spells").value = intern[3].lvl8spells
-        document.getElementById("lvl9spells").value = intern[3].lvl9spells
+        document.getElementById("spell_save").value = stat[3].spellsave
+        document.getElementById("spell_atk_bonus").value = stat[3].spellattack
+        document.getElementById("cantrips").value = stat[3].cantrips
+        document.getElementById("lvl1spells").value = stat[3].lvl1spells
+        document.getElementById("lvl2spells").value = stat[3].lvl2spells
+        document.getElementById("lvl3spells").value = stat[3].lvl3spells
+        document.getElementById("lvl4spells").value = stat[3].lvl4spells
+        document.getElementById("lvl5spells").value = stat[3].lvl5spells
+        document.getElementById("lvl6spells").value = stat[3].lvl6spells
+        document.getElementById("lvl7spells").value = stat[3].lvl7spells
+        document.getElementById("lvl8spells").value = stat[3].lvl8spells
+        document.getElementById("lvl9spells").value = stat[3].lvl9spells
     }
     fileread.readAsText(loaded_data);
 }
